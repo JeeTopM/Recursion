dd70fcbee19aea9df31941dec081d9109897075e
Лев Романович Панас <jieba2005@list.ru>
2025-04-30 13:11:23 +0300

    Start recursion lesson: setup files, examples, and notes

---
+++ Part 3 - Recursion and Memoization/test-10.py
@@ -0,0 +1,14 @@
+"""
+Функция is_palindrome()
+Реализуйте функцию is_palindrome() с использованием рекурсии, которая принимает один аргумент:
+string — произвольная строка
+Функция должна возвращать значение True, если переданная строка является палиндромом, или False в противном случае.
+"""
+
+
+def is_palindrome():
+    pass
+
+
+print(is_palindrome("stepik"))  # False
+print(is_palindrome("level"))  # True
